36ab22a43a8264bd55938f94d3835eb68345bb89
df.riveros11 <df.riveros11@ISIS2603S2-0023.sis.virtual.uniandes.edu.co>
2017-09-12 14:03:18 -0500

    Listo

---
+++ nboletas-back/src/main/java/co/edu/uniandes/theexceptions/nboletas/ejb/AbstractLogic.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.theexceptions.nboletas.ejb;
+
+import java.util.List;
+
+/**
+ *
+ * @author df.riveros11
+ * @param <T>
+ */
+public abstract class AbstractLogic<T> {
+    /**Crear un objeto tipo T
+     * @param entity    
+     * @return     
+    */
+    public abstract T create(T entity);
+    
+    /**Actualiza un obeto de tipo T 
+     * @param entity    
+     * @return     
+    */
+    public abstract T uptade(T entity);
+    
+    /**Elimina un objeto T
+     * @param entity    
+    */
+    public abstract void delete(T entity);
+    
+    /**Encuentra un tipo T al mandar un identificador
+     * @param id    
+     * @return     
+    */
+    public abstract T find(Object id);
+    
+    /**Devuelve todo los objetos
+     * @return list con todos los objetos
+    */
+    public abstract List<T> findAll();
+
+}
