3bf0671626d207fa17e0a66f28496b51048cd07d
jf.ramos <jf.ramos@ISIS2603S2-0040.sis.virtual.uniandes.edu.co>
2017-09-17 21:07:59 -0500

    Creacion inicial de las clases necesarias para implementar servicios REST

--- nboletas-back/src/main/java/co/edu/uniandes/theexceptions/nboletas/ejb/BoletaLogic.java
+++ nboletas-back/src/main/java/co/edu/uniandes/theexceptions/nboletas/ejb/BoletaLogic.java
@@ -92,4 +92,8 @@ public class BoletaLogic extends AbstractLogic<BoletaEntity> {
         return boletas;
     }
 
+    public List<BoletaEntity> getBoletas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
---
+++ nboletas-back/src/main/java/co/edu/uniandes/theexceptions/nboletas/ejb/EspectaculoLogic.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.theexceptions.nboletas.ejb;
+
+import co.edu.uniandes.theexceptions.nboletas.entities.EspectaculoEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author jf.ramos
+ */
+@Stateless
+public class EspectaculoLogic extends AbstractLogic<EspectaculoEntity> {
+
+    @Override
+    public EspectaculoEntity create(EspectaculoEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public EspectaculoEntity uptade(EspectaculoEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(EspectaculoEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public EspectaculoEntity find(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<EspectaculoEntity> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
