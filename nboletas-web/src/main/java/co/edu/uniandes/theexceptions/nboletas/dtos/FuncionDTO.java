6c18a90a6802d44cde467c36eb809d259199503e
ja.gomez1 <ja.gomez1@ISIS2603S2-0039.sis.virtual.uniandes.edu.co>
2017-09-19 21:27:19 -0500

    Lugar y Funcion (DTO, DetailDTO)

---
+++ nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/dtos/FuncionDTO.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.theexceptions.nboletas.dtos;
+
+import co.edu.uniandes.theexceptions.nboletas.entities.FuncionEntity;
+import java.util.Date;
+
+/**
+ *
+ * @author ja.gomez1
+ */
+public class FuncionDTO {
+    private Long id;
+    private Date fecha;
+
+    public FuncionDTO() {
+    }
+
+    public FuncionDTO(FuncionEntity entity) {
+        this.id = entity.getId();
+        this.fecha = entity.getFecha();
+    }
+    
+    public FuncionEntity toEntity() {
+        FuncionEntity entity = new FuncionEntity();
+        entity.setId(this.getId());
+        entity.setFecha(this.getFecha());
+        return entity;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
