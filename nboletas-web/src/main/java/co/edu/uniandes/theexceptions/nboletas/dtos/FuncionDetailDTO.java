bbbaf93de7a09f7da91e0024e9994aa146213f00
ja.gomez1 <ja.gomez1@ISIS2603S2-0039.sis.virtual.uniandes.edu.co>
2017-09-22 19:59:08 -0500

    Update DetailDTOs

--- nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/dtos/FuncionDetailDTO.java
+++ nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/dtos/FuncionDetailDTO.java
@@ -5,23 +5,48 @@
  */
 package co.edu.uniandes.theexceptions.nboletas.dtos;
 
+import co.edu.uniandes.theexceptions.nboletas.entities.BoletaEntity;
+import co.edu.uniandes.theexceptions.nboletas.entities.DivisionDeLugarEntity;
 import co.edu.uniandes.theexceptions.nboletas.entities.FuncionEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author ja.gomez1
  */
 public class FuncionDetailDTO extends FuncionDTO {
+    
+    private List<BoletaDTO> boletas;
+    
+    private LugarDTO lugar;
+    
+    private EspectaculoDTO espectaculo;
 
     public FuncionDetailDTO() {
     }
 
     public FuncionDetailDTO(FuncionEntity entity) {
         super(entity);
+        
+        if(entity.getBoletas()!= null) {
+            ArrayList<BoletaDTO> b = new ArrayList<>();
+            for(BoletaEntity e : entity.getBoletas()) b.add(new BoletaDTO(e));
+            this.boletas = b;
+        }
+        
+        if(entity.getLugar() != null) {
+            this.lugar = new LugarDTO(entity.getLugar());
+        }
+        
+        if(entity.getEspectaculo()!= null) {
+            this.espectaculo = new EspectaculoDTO(entity.getEspectaculo());
+        }
     }
     
     @Override
     public FuncionEntity toEntity() {
-        return super.toEntity();
+        FuncionEntity f = super.toEntity();
+        return f;
     }
 }
--- nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/dtos/LugarDetailDTO.java
+++ nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/dtos/LugarDetailDTO.java
@@ -5,23 +5,87 @@
  */
 package co.edu.uniandes.theexceptions.nboletas.dtos;
 
+import co.edu.uniandes.theexceptions.nboletas.entities.DivisionDeLugarEntity;
+import co.edu.uniandes.theexceptions.nboletas.entities.FuncionEntity;
 import co.edu.uniandes.theexceptions.nboletas.entities.LugarEntity;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  *
  * @author ja.gomez1
  */
 public class LugarDetailDTO extends LugarDTO {
+    
+    private List<DivisionDeLugarDTO> divisiones;
+    
+    private List<FuncionDTO> funciones;
 
     public LugarDetailDTO() {
     }
 
     public LugarDetailDTO(LugarEntity entity) {
         super(entity);
+        
+        if(entity.getDivisiones() != null) {
+            ArrayList<DivisionDeLugarDTO> d = new ArrayList<>();
+            for(DivisionDeLugarEntity e : entity.getDivisiones()) d.add(new DivisionDeLugarDTO(e));
+            this.divisiones = d;
+        }
+        
+        if(entity.getFunciones() != null) {
+            ArrayList<FuncionDTO> f = new ArrayList<>();
+            for(FuncionEntity e : entity.getFunciones()) f.add(new FuncionDTO(e));
+            this.funciones = f;
+        }
     }
     
     @Override
     public LugarEntity toEntity() {
-        return super.toEntity();
+        LugarEntity l = super.toEntity();
+        
+        if(getDivisiones() != null) {
+            ArrayList<DivisionDeLugarEntity> d = new ArrayList<DivisionDeLugarEntity>();
+            for(DivisionDeLugarDTO p : getDivisiones()) d.add(p.toEntity());
+            l.setDivisiones(d);
+        }
+        
+        if(getFunciones() != null) {
+            ArrayList<FuncionEntity> f = new ArrayList<FuncionEntity>();
+            for(FuncionDTO p : getFunciones()) f.add(p.toEntity());
+            l.setFunciones(f);
+        }
+        return l;
+    }
+
+    /**
+     * @return the divisiones
+     */
+    public List<DivisionDeLugarDTO> getDivisiones() {
+        return divisiones;
+    }
+
+    /**
+     * @param divisiones the divisiones to set
+     */
+    public void setDivisiones(List<DivisionDeLugarDTO> divisiones) {
+        this.divisiones = divisiones;
+    }
+
+    /**
+     * @return the funciones
+     */
+    public List<FuncionDTO> getFunciones() {
+        return funciones;
+    }
+
+    /**
+     * @param funciones the funciones to set
+     */
+    public void setFunciones(List<FuncionDTO> funciones) {
+        this.funciones = funciones;
     }
 }
