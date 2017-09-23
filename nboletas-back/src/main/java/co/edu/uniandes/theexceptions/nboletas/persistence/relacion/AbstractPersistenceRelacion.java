efa008fbc68ac34c13eafd4dfa73a341b4b9fe7e
ja.gomez1 <ja.gomez1@ISIS2603S2-0039.sis.virtual.uniandes.edu.co>
2017-09-22 22:39:26 -0500

    Romper la regla, esto no funciona.

--- nboletas-back/src/main/java/co/edu/uniandes/theexceptions/nboletas/persistence/relacion/AbstractPersistenceRelacion.java
+++ nboletas-back/src/main/java/co/edu/uniandes/theexceptions/nboletas/persistence/relacion/AbstractPersistenceRelacion.java
@@ -6,6 +6,10 @@
 package co.edu.uniandes.theexceptions.nboletas.persistence.relacion;
 
 import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -13,9 +17,24 @@ import java.util.List;
  * @param <T>
  * @param <P>
  */
+@Stateless
 public class AbstractPersistenceRelacion <T, P>{
     
+    private final Class<T> entityClassT;
+    private final Class<P> entityClassP;
+            
+    public AbstractPersistenceRelacion(Class<T> entityClassT, Class<P> entityClassP){
+        this.entityClassT = entityClassT; 
+        this.entityClassP = entityClassP;
+    }
+    
+      
+    @PersistenceContext(unitName = "nboletasPU")
+    private EntityManager em;
+    
     public P create(Object id, P entity){
+        em.createQuery("INSERT INTO c values (c)", entity.getClass());
+        em.createQuery("INSERT INTO d values ")
         return null;
     }
     
