4e7fa4819cb30644040408090e9c5b265e50246e
jf.ramos <jf.ramos@ISIS2603S2-0040.sis.virtual.uniandes.edu.co>
2017-09-25 14:06:05 -0500

    cambio en sql

--- nboletas-web/sql/nboletas.sql
+++ nboletas-web/sql/nboletas.sql
@@ -2,16 +2,18 @@ delete from BoletaEntity;
 delete from OrganizadorEntity;
 delete from EnvioEntity;
 delete from ComentarioEntity;
+delete from EspectaculoEntity;
+delete from ArtistaEntity;
 
-insert into BoletaEntity (id, precio, vendida) values (1, 100.23, false);
-insert into BoletaEntity (id, precio, vendida) values (2, 20.23, true);
-insert into BoletaEntity (id, precio, vendida) values (3, 0.23, false);
-insert into BoletaEntity (id, precio, vendida) values (4, 0.00, true);
+insert into BoletaEntity (id, precio, vendida) values (1, 100.23, 0);
+insert into BoletaEntity (id, precio, vendida) values (2, 20.23, 1);
+insert into BoletaEntity (id, precio, vendida) values (3, 0.23, 0);
+insert into BoletaEntity (id, precio, vendida) values (4, 0.00, 1);
 
-insert into OrganizadorEntity (id, nombreEmpresa) values (1, "Yo");
-insert into OrganizadorEntity (id, nombreEmpresa) values (2, "YoYQue");
-insert into OrganizadorEntity (id, nombreEmpresa) values (3, "DondeEstoy");
-insert into OrganizadorEntity (id, nombreEmpresa) values (4, "TuOtraVez");
+insert into OrganizadorEntity (id, nombreEmpresa) values (1, 'Yo');
+insert into OrganizadorEntity (id, nombreEmpresa) values (2, 'YoYQue');
+insert into OrganizadorEntity (id, nombreEmpresa) values (3, 'DondeEstoy');
+insert into OrganizadorEntity (id, nombreEmpresa) values (4, 'TuOtraVez');
 
 
 insert into EnvioEntity (id, direccion) values (100,'Calle 43 #7-26 - Aprt:101');
@@ -23,3 +25,14 @@ insert into ComentarioEntity (id,comentario,fecha) values (100,'Muy buen especta
 insert into ComentarioEntity (id,comentario,fecha) values (200,'horror de evento','10/31/2017');
 insert into ComentarioEntity (id,comentario,fecha) values (300,'me gusto mucho asistir','11/01/1989');
 insert into ComentarioEntity (id,comentario,fecha) values (400,'me senti muy bien en el evento','06/20/2017');
+
+insert into ArtistaEntity (id, nombreArtista) values (1, 'Juanes');
+insert into ArtistaEntity (id, nombreArtista) values (2, 'Shakira');
+insert into ArtistaEntity (id, nombreArtista) values (3, 'Cirque du Soleil');
+insert into ArtistaEntity (id, nombreArtista) values (4, 'Portugal The Man');
+
+
+insert into EspectaculoEntity (id, nombre, descripcion) values (1, 'Septimo dia', 'Espectaculo de Cirque Du Soleil junto a Soda Estereo');
+insert into EspectaculoEntity (id, nombre, descripcion) values (2, 'P.A.R.C.E', 'Gira de Juanes pro Colombia');
+insert into EspectaculoEntity (id, nombre, descripcion) values (3, 'Concierto Shakira', 'Concierto');
+insert into EspectaculoEntity (id, nombre, descripcion) values (4, 'Gira Portugal The Man', 'Concierto');
