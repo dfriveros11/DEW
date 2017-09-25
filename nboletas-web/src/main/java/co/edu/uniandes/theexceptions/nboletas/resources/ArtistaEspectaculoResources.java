0985a3d7c1b491417b777166e1314cc82ad8f093
jf.ramos <jf.ramos@ISIS2603S2-0040.sis.virtual.uniandes.edu.co>
2017-09-25 14:07:31 -0500

    cambio en sql

---
+++ nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/resources/ArtistaEspectaculoResources.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.theexceptions.nboletas.resources;
+
+
+import co.edu.uniandes.theexceptions.nboletas.dtos.EspectaculoDetailDTO;
+import co.edu.uniandes.theexceptions.nboletas.ejb.ArtistaLogic;
+import co.edu.uniandes.theexceptions.nboletas.ejb.EspectaculoLogic;
+import co.edu.uniandes.theexceptions.nboletas.entities.ArtistaEntity;
+import co.edu.uniandes.theexceptions.nboletas.entities.EspectaculoEntity;
+import co.edu.uniandes.theexceptions.nboletas.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author jf.ramos
+ */
+@Path("artistas/{idArtista: \\d+}/espectaculos")
+@Produces("application/json")
+@Consumes("application/json")
+@Stateless
+public class ArtistaEspectaculoResources {
+   
+@Inject
+    ArtistaLogic artistaLogic;
+
+@Inject
+    EspectaculoLogic espectaculoLogic;
+
+    @POST
+    public EspectaculoDetailDTO createArtistaEspectaculo(@PathParam("idArtista") Long idArtista, EspectaculoDetailDTO espectaculo) throws BusinessLogicException {
+        ArtistaEntity artista = artistaLogic.find(idArtista);
+        if (artista == null) {
+            throw new BusinessLogicException("No existe el artista con el id: " + idArtista);
+        }
+        EspectaculoEntity espectaculoE = espectaculo.toEntity();
+        List<EspectaculoEntity> espectaculos = new ArrayList<>();
+        espectaculos.add(espectaculoE);
+        artista.setEspectaculos(espectaculos);
+        EspectaculoEntity espectaculoCreado = espectaculoLogic.create(espectaculoE);
+        return new EspectaculoDetailDTO(espectaculoCreado);
+    }
+
+    @GET
+    public List<EspectaculoDetailDTO> getEspectaculos(@PathParam("idArtista") Long idArtista) throws BusinessLogicException {
+        List<EspectaculoEntity> list = new ArrayList<>();
+        ArtistaEntity artista = artistaLogic.find(idArtista);
+        if (artista == null) {
+            throw new BusinessLogicException("No existe el artista con ese id: " + idArtista);
+        }
+        List<EspectaculoEntity> espectaculos = artista.getEspectaculos();
+        if (espectaculos != null) {
+            for (EspectaculoEntity espectaculo : espectaculos) {
+                list.add(espectaculo);
+            }
+        }
+        return listEntity2DetailDTO(list);
+    }
+
+    @GET
+    @Path("{idEspectaculo: \\d+}")
+    public EspectaculoDetailDTO getEspectaculo(@PathParam("idArtista") Long idArtista, @PathParam("idEspectaculo") Long idEspectaculo) throws BusinessLogicException {
+        ArtistaEntity artista = artistaLogic.find(idArtista);
+        if (artista == null) {
+            throw new BusinessLogicException("No existe el artista con ese id: " + idArtista);
+        }
+        EspectaculoEntity espectaculo = espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con ese id: " + idEspectaculo);
+        }
+        return new EspectaculoDetailDTO(espectaculo);
+    }
+
+    @PUT
+    @Path("{idEspectaculo: \\d+}")
+    public EspectaculoDetailDTO updateArtistaEspectaculo(@PathParam("idArtista") Long idArtista, @PathParam("idEspectaculo") Long idEspectaculo, EspectaculoDetailDTO espectaculo ) throws BusinessLogicException {
+        ArtistaEntity artista = artistaLogic.find(idArtista);
+        if (artista == null) {
+            throw new BusinessLogicException("No existe el artista con ese id: " + idArtista);
+        }
+        if (null == espectaculoLogic.find(idEspectaculo)) {
+            throw new BusinessLogicException("No existe el organizador con ese id: " + idEspectaculo);
+        }
+        EspectaculoEntity espectaculoActualizar = espectaculo.toEntity();
+        List<ArtistaEntity> artistas = new ArrayList<>();
+        artistas.add(artista);
+        espectaculoActualizar.setArtista(artistas);
+        espectaculoActualizar.setId(idEspectaculo);
+        EspectaculoEntity actual = espectaculoLogic.update(espectaculoActualizar);
+        return new EspectaculoDetailDTO(actual);
+    }
+
+    @DELETE
+    @Path("{idEspectaculo: \\d+}")
+    public void deleteArtistaEspectaculo(@PathParam("idArtista") Long idArtista, @PathParam("idEspectaculo") Long idEspectaculo) throws BusinessLogicException {
+        ArtistaEntity artista = artistaLogic.find(idArtista);
+        if (artista == null) {
+            throw new BusinessLogicException("No existe el artista con ese id: " + idArtista);
+        }
+        EspectaculoEntity espectaculo = espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con ese id: " + idEspectaculo);
+        }
+        List<ArtistaEntity> artistas= new ArrayList<>();
+        artistas.add(artista);
+        espectaculo.setArtista(artistas);
+        espectaculoLogic.delete(espectaculo);
+    }
+
+    private List<EspectaculoDetailDTO> listEntity2DetailDTO(List<EspectaculoEntity> entityList) {
+        List<EspectaculoDetailDTO> list = new ArrayList<>();
+        for (EspectaculoEntity entity : entityList) {
+            list.add(new EspectaculoDetailDTO(entity));
+        }
+        return list;
+    }
+}
+
--- nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/resources/EspectaculoComentarioResouce.java
+++ nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/resources/EspectaculoComentarioResouce.java
@@ -5,10 +5,120 @@
  */
 package co.edu.uniandes.theexceptions.nboletas.resources;
 
+import co.edu.uniandes.theexceptions.nboletas.dtos.ComentarioDetailDTO;
+import co.edu.uniandes.theexceptions.nboletas.ejb.ComentarioLogic;
+import co.edu.uniandes.theexceptions.nboletas.ejb.EspectaculoLogic;
+import co.edu.uniandes.theexceptions.nboletas.entities.ComentarioEntity;
+import co.edu.uniandes.theexceptions.nboletas.entities.EspectaculoEntity;
+import co.edu.uniandes.theexceptions.nboletas.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author jf.ramos
  */
+@Path("espectaculos/{idEspectaculo: \\d+}/comentarios")
+@Produces("application/json")
+@Consumes("application/json")
+@Stateless
 public class EspectaculoComentarioResouce {
+    @Inject
+    EspectaculoLogic espectaculoLogic;
     
+    @Inject
+    ComentarioLogic comentarioLogic;
+    
+    @POST
+    public ComentarioDetailDTO createEspectaculoComentario(@PathParam("idEspectaculo") Long idEspectaculo, ComentarioDetailDTO comentario) throws BusinessLogicException {
+        EspectaculoEntity espectaculo = espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con el id: " + idEspectaculo);
+        }
+        ComentarioEntity comentarioEntity = comentario.toEntity();
+        comentarioEntity.setEspectaculo(espectaculo);
+        ComentarioEntity comentarioCreado = comentarioLogic.create(comentarioEntity);
+        return new ComentarioDetailDTO(comentarioCreado);
+    }
+    
+    @GET
+    public List<ComentarioDetailDTO> getEspectaculoComentarios(@PathParam("idEspectaculo") Long idEspectaculo) throws BusinessLogicException {
+        List<ComentarioEntity> list = new ArrayList<>();
+        EspectaculoEntity espectaculo = espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con ese id: " + idEspectaculo);
+        }
+        List<ComentarioEntity> comentarios = espectaculo.getComentarios();
+        if(comentarios!=null){
+        for (ComentarioEntity comentarioEntity : comentarios) {
+            list.add(comentarioEntity);
+        }
+        }
+        return listEntity2DetailDTO(list);
+    }
+    
+    @GET
+    @Path("{idComentario: \\d+}")
+    public ComentarioDetailDTO getEspectaculoComentario(@PathParam("idEspectaculo") Long idEspectaculo, @PathParam("idComentario") Long idComentario) throws BusinessLogicException {
+        EspectaculoEntity espectaculo= espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con ese id: " + idEspectaculo);
+        }
+        ComentarioEntity comentario = comentarioLogic.find(idComentario);
+        if (comentario == null) {
+            throw new BusinessLogicException("No existe el comentario con ese id: " + idComentario);
+        }
+        comentario.setEspectaculo(espectaculo);
+        return new ComentarioDetailDTO(comentario);
+    }
+    
+    @PUT
+    @Path("{idComentario: \\d+}")
+    public ComentarioDetailDTO updateEspectaculoComentario(@PathParam("idEspectaculo") Long idEspectaculo, @PathParam("idComentario") Long idComentario, ComentarioDetailDTO comentario) throws BusinessLogicException {
+        EspectaculoEntity espectaculo = espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con ese id: " + idEspectaculo);
+        }
+        if (null == comentarioLogic.find(idComentario)) {
+            throw new BusinessLogicException("No existe el comentario con ese id: " + idComentario);
+        }
+        ComentarioEntity comentarioActualizar = comentario.toEntity();
+        comentarioActualizar.setEspectaculo(espectaculo);
+        comentarioActualizar.setId(idComentario);
+        ComentarioEntity actual = comentarioLogic.update(comentarioActualizar);
+        return new ComentarioDetailDTO(actual);
+    }
+    
+    @DELETE
+    @Path("{idComentario: \\d+}")
+    public void deleteEspectaculoComentario(@PathParam("idEspectaculo") Long idEspectaculo, @PathParam("idComentario") Long idComentario) throws BusinessLogicException {
+        EspectaculoEntity espectaculo = espectaculoLogic.find(idEspectaculo);
+        if (espectaculo == null) {
+            throw new BusinessLogicException("No existe el espectaculo con ese id: " + idEspectaculo);
+        }
+        ComentarioEntity comentario = comentarioLogic.find(idComentario);
+        if (comentario == null) {
+            throw new BusinessLogicException("No existe el comentario con ese id: " + idComentario);
+        }
+        comentario.setEspectaculo(espectaculo);
+        comentarioLogic.delete(comentario);
+    }
+
+    private List<ComentarioDetailDTO> listEntity2DetailDTO(List<ComentarioEntity> entityList) {
+        List<ComentarioDetailDTO> list = new ArrayList<>();
+        for (ComentarioEntity entity : entityList) {
+            list.add(new ComentarioDetailDTO(entity));
+        }
+        return list;
+    }
 }
