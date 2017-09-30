abf4a5f78efcf481a02ec83f59a869df162da568
df.riveros11 <df.riveros11@ISIS2603S2-0023.sis.virtual.uniandes.edu.co>
2017-09-29 22:47:34 -0500

    pruebas

--- nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/resources/BoletaEnvioResource.java
+++ nboletas-web/src/main/java/co/edu/uniandes/theexceptions/nboletas/resources/BoletaEnvioResource.java
@@ -11,8 +11,12 @@ import co.edu.uniandes.theexceptions.nboletas.ejb.EnvioLogic;
 import co.edu.uniandes.theexceptions.nboletas.entities.BoletaEntity;
 import co.edu.uniandes.theexceptions.nboletas.entities.EnvioEntity;
 import co.edu.uniandes.theexceptions.nboletas.exceptions.BusinessLogicException;
+import com.gs.collections.impl.list.fixed.ArrayAdapter;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -42,86 +46,46 @@ public class BoletaEnvioResource {
 
     @POST
     public EnvioDetailDTO createBoletaEnvio(@PathParam("idBoleta") Long idBoleta, EnvioDetailDTO envio) throws BusinessLogicException {
-        BoletaEntity boleta = boletaLogic.find(idBoleta);
-        if (boleta == null) {
-            throw new BusinessLogicException("No existe la boleta con el id: " + idBoleta);
-        }
-        EnvioEntity envioE = envio.toEntity();
-        envioE.setBoleta(boleta);
-        EnvioEntity envioCreado = envioLogic.create(envioE);
+        EnvioEntity envioCreado = boletaLogic.createBoletaEnvio(idBoleta, envio.toEntity());
         return new EnvioDetailDTO(envioCreado);
     }
 
     @GET
     public List<EnvioDetailDTO> getEnvios(@PathParam("idBoleta") Long idBoleta) throws BusinessLogicException {
-        List<EnvioEntity> list = new ArrayList<>();
-        BoletaEntity boleta = boletaLogic.find(idBoleta);
-        if (boleta == null) {
-            throw new BusinessLogicException("No existe la boleta con ese id: " + idBoleta);
+        List<EnvioEntity> envios = new ArrayList();
+        try{
+            EnvioEntity envio  = boletaLogic.find(idBoleta).getEnvio();
+            envios.add(envio);
+        }catch(Exception e){
+            throw new BusinessLogicException("No se encuentra la boleta con el id: " + idBoleta + " mayor infromacion: " + e.getMessage());
         }
-        EnvioEntity envio = boleta.getEnvio();
-        if (envio != null) {
-            list.add(boleta.getEnvio());
-        }
-        return listEntity2DetailDTO(list);
+        EnvioDetailDTO entrega = new EnvioDetailDTO();
+        return entrega.listEnvioEntity2EnvioDetailDTO(envios);
     }
 
     @GET
     @Path("{idEnvio: \\d+}")
-    public EnvioDetailDTO getEnvio(@PathParam("idBoleta") Long idBoleta, @PathParam("idEnvio") Long idEnvio) throws BusinessLogicException {
-        BoletaEntity boleta = boletaLogic.find(idBoleta);
-        if (boleta == null) {
-            throw new BusinessLogicException("No existe la boleta con ese id: " + idBoleta);
+    public EnvioDetailDTO getEnvio(@PathParam("idBoleta") Long idBoleta, @PathParam("idEnvio") Long idEnvio) {
+        EnvioEntity envio = null;
+        try {
+            envio = boletaLogic.findBoletaEnvio(idBoleta, idEnvio);
+        } catch (BusinessLogicException ex) {
+            Logger.getLogger(BoletaEnvioResource.class.getName()).log(Level.SEVERE, null, ex);
         }
-        EnvioEntity envio = envioLogic.find(idEnvio);
-        if (envio == null) {
-            throw new BusinessLogicException("No existe el envio con ese id: " + idEnvio);
-        }
-        long id = envio.getBoleta().getId();
-        if(id != idBoleta){
-             throw new BusinessLogicException("No existe la relacion ");
-        }
-        envio.setBoleta(boleta);
         return new EnvioDetailDTO(envio);
     }
 
     @PUT
     @Path("{idEnvio: \\d+}")
     public EnvioDetailDTO updateBoletaEnvio(@PathParam("idBoleta") Long idBoleta, @PathParam("idEnvio") Long idEnvio, EnvioDetailDTO envio) throws BusinessLogicException {
-        BoletaEntity boleta = boletaLogic.find(idBoleta);
-        if (boleta == null) {
-            throw new BusinessLogicException("No existe la boleta con ese id: " + idBoleta);
-        }
-        if (null == envioLogic.find(idEnvio)) {
-            throw new BusinessLogicException("No existe el envio con ese id: " + idEnvio);
-        }
-        EnvioEntity envioActualizar = envio.toEntity();
-        envioActualizar.setBoleta(boleta);
-        envioActualizar.setId(idEnvio);
-        EnvioEntity actual = envioLogic.update(envioActualizar);
+        EnvioEntity actual = boletaLogic.updateBoletaEnvio(idBoleta, idEnvio, envio.toEntity());
         return new EnvioDetailDTO(actual);
     }
 
     @DELETE
     @Path("{idEnvio: \\d+}")
     public void deleteBoletaEnvio(@PathParam("idBoleta") Long idBoleta, @PathParam("idEnvio") Long idEnvio) throws BusinessLogicException {
-        BoletaEntity boleta = boletaLogic.find(idBoleta);
-        if (boleta == null) {
-            throw new BusinessLogicException("No existe la boleta con ese id: " + idBoleta);
-        }
-        EnvioEntity envio = envioLogic.find(idEnvio);
-        if (envio == null) {
-            throw new BusinessLogicException("No existe el envio con ese id: " + idEnvio);
-        }
-        envio.setBoleta(boleta);
-        envioLogic.delete(envio);
+        boletaLogic.deleteBoletaEnvio(idBoleta, idEnvio);
     }
-
-    private List<EnvioDetailDTO> listEntity2DetailDTO(List<EnvioEntity> entityList) {
-        List<EnvioDetailDTO> list = new ArrayList<>();
-        for (EnvioEntity entity : entityList) {
-            list.add(new EnvioDetailDTO(entity));
-        }
-        return list;
-    }
+    
 }
