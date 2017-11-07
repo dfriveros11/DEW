c16de8c9416699d10c44766cc90617dcb4d16856
Jorge Andres Gomez <ja.gomez1@uniandes.edu.co>
2017-11-07 06:50:17 -0500

    Ctrl funcion scope fix

--- nboletas-web/src/main/webapp/src/modules/funciones/update/funciones.update.ctrl.js
+++ nboletas-web/src/main/webapp/src/modules/funciones/update/funciones.update.ctrl.js
@@ -10,11 +10,7 @@
                     var idFuncion = $state.params.funcionId;
                     
                     $http.get(funcionesContext + '/' + idFuncion).then(function (response) {
-                        var funcion = response.data;
-                        $scope.funcion.fecha = funcion.fecha;
-                        $scope.funcion.espectaculo = funcion.espectaculo;
-                        $scope.funcion.lugar = funcion.lugar;
-                        $scope.funcion.hora = funcion.hora;
+                        $scope.funcion = response.data;
                     });
                 $scope.updateFuncion = function () {
                     $http.put(funcionesContext + "/" + idFuncion, $scope.funcion).then(function (response) {
