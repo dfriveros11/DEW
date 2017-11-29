c9ca8bccdafc95cb38a5709d6a80da1642e4599c
Jorge Andres Gomez <ja.gomez1@uniandes.edu.co>
2017-11-29 01:20:49 -0500

    Inicio sql display

---
+++ nboletas-web/sql/nboletas_display.sql
@@ -0,0 +1,29 @@
+delete from ReembolsoEntity;
+delete from EnvioEntity;
+delete from ComentarioEntity;
+delete from BoletaEntity;
+delete from OrganizadorEntity;
+delete from FuncionEntity;
+delete from EspectaculoEntity;
+delete from ArtistaEntity;
+delete from SillaEntity;
+delete from DivisionDeLugarEntity;
+delete from LugarEntity;
+delete from EspectaculoEntity_OrganizadorEntity;
+delete from UsuarioEntity;
+
+insert into EspectaculoEntity (id, nombre, descripcion, imagen) values (1, 'Septimo dia', 'Espectaculo de Cirque Du Soleil junto a Soda Estereo', 'https://puntoticket-a.akamaihd.net/especiales/septimo-dia/img/septimo-dia.jpg');
+insert into EspectaculoEntity (id, nombre, descripcion, imagen) values (2, 'P.A.R.C.E', 'Gira de Juanes por Colombia', 'http://images.coveralia.com/audio/j/Juanes-P_A_R_C_E_(Deluxe_Edition)-Frontal.jpg');
+insert into EspectaculoEntity (id, nombre, descripcion, imagen) values (3, 'Concierto Shakira', 'Concierto', 'https://www.music-bazaar.com/album-images/vol8/461/461159/2278937-big/Shakira-Live-From-Paris-cover.jpg');
+insert into EspectaculoEntity (id, nombre, descripcion, imagen) values (4, 'Gira Portugal The Man', 'Concierto', 'http://revistakuadro.com/wp-content/uploads/2015/03/b9185c948d.jpg');
+
+insert into ArtistaEntity (id, nombre, imagen) values (1, 'Juanes', 'http://d3iln1l77n73l7.cloudfront.net/couch_images/attachments/000/083/784/original/JUANES.jpg?2016');
+insert into ArtistaEntity (id, nombre, imagen) values (2, 'Shakira', 'https://www.google.com.co/search?q=shakira&rlz=1C5CHFA_enCO729CO729&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiDpMOopuLXAhXG5CYKHQ6tBzcQ_AUICygC&biw=1164&bih=618#imgrc=kIkRoZ4sqbm3vM:');
+insert into ArtistaEntity (id, nombre, imagen) values (3, 'Cirque du Soleil', 'https://www.cirquedusoleil.com/-/media/past-shows/assets/img/iris/slider/iris-act-kiriki.jpg');
+insert into ArtistaEntity (id, nombre, imagen) values (4, 'Portugal The Man', 'https://www.google.com.co/search?rlz=1C5CHFA_enCO729CO729&tbm=isch&q=portugal+the+man+concierto&spell=1&sa=X&ved=0ahUKEwiRoJWbpeLXAhUB4iYKHRPbBYcQvwUIISgA&biw=1164&bih=618&dpr=2.2#imgrc=GLE7Djxg9_3pIM:');
+
+insert into 
+
+
+
+
